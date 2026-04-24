bfdd17a3d5caeba106fcba36af7748f13aa86945
gtet <sundararao.b@narayanagroup.com>
2026-04-25 00:52:14 +0530

    config: responsive setup

--- next_blogs/src/app/page.tsx
+++ next_blogs/src/app/page.tsx
@@ -1,9 +1,15 @@
-import ThemeToggle from "@/components/ThemeToggle";
+import Container from "@/components/Container";
 
 export default function Page() {
   return (
-    <main className="min-h-screen p-8">
-      <ThemeToggle />
+    <main className="min-h-screen">
+      <Container>
+        <div className="flex flex-wrap">
+          <div className="hidden min-[960px]:block w-1/6" id="left-section">jlkj</div>
+          <div className="w-1/2" id="main-section">ajkflajf</div>
+          <div className="w-1/3" id='right-section'>jafljaf</div>
+        </div>
+      </Container>
     </main>
   );
 }
