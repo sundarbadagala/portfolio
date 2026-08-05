92ff32b2d6511a5b005322cf6a381990238144aa
sundar rao <sundar.badagala@gmail.com>
2026-08-05 21:46:32 +0530

    code editor issues fix

--- react_web/src/share/organisms/PreviewContent/index.tsx
+++ react_web/src/share/organisms/PreviewContent/index.tsx
@@ -20,7 +20,18 @@ function PreviewContent({ title, children, tags }: any) {
       <Box style={{ margin: "24px 0" }}>
         <hr />
       </Box>
-      <div dangerouslySetInnerHTML={{ __html: children }} />
+      <Box
+        sx={{
+          "h1, h2, h3, h4, h5, h6, ul, ol, li, p, blockquote, code": {
+            all: "revert",
+          },
+          "prev": {
+            width: "100%"
+          }
+
+        }}
+        dangerouslySetInnerHTML={{ __html: children }}
+      />
     </>
   );
 }
